e1059cdb9fc8c5bdbeae94940279068a8b6b0f5e
Arihan Yadav <57842655+Air-crypto@users.noreply.github.com>
2024-10-11 12:41:55 -0700

    Update Q2.sql

--- Stage 2/Q2.sql
+++ Stage 2/Q2.sql
@@ -1,1 +1,5 @@
-
+SELECT d.department_name, COUNT(e.employee_id) as employee_count
+FROM departments d
+JOIN employees e ON d.department_id = e.department_id
+GROUP BY d.department_name
+ORDER BY employee_count DESC;
