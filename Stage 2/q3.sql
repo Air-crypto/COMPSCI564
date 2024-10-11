0c22baf15caeab6cb1088d18a33361824419ec01
Anna <162251355+aahuanga@users.noreply.github.com>
2024-10-11 15:03:56 -0500

    Update q3.sql

--- Stage 2/q3.sql
+++ Stage 2/q3.sql
@@ -1,1 +1,6 @@
-
+SELECT d.department_name, AVG(j.max_salary) AS avg_max_salaries
+FROM departments d
+JOIN employees e ON d.department_id = e.department_id 
+JOIN jobs j ON e.job_id = j.job_id
+GROUP BY d.department_name
+HAVING avg_max_salaries > 8000;
