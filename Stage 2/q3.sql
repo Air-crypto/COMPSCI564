a25231b336a065ccd7671f605fa7a4163131e54a
Arihan Yadav <57842655+Air-crypto@users.noreply.github.com>
2024-10-11 13:23:01 -0700

    Update q3.sql

--- Stage 2/q3.sql
+++ Stage 2/q3.sql
@@ -1,6 +1,6 @@
-SELECT d.department_name, AVG(j.max_salary) AS avg_max_salaries
+SELECT d.department_name, AVG(j.max_salary) AS avg_max_salary
 FROM departments d
-JOIN employees e ON d.department_id = e.department_id 
+JOIN employees e ON d.department_id = e.department_id
 JOIN jobs j ON e.job_id = j.job_id
 GROUP BY d.department_name
-HAVING avg_max_salaries > 8000;
+HAVING avg_max_salary > 8000;
