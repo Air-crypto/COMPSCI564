466201f02859edeea95d45b2b265139ec14b5894
Anna <162251355+aahuanga@users.noreply.github.com>
2024-10-11 14:57:47 -0500

    Update q6.sql

--- Stage 2/q6.sql
+++ Stage 2/q6.sql
@@ -1,1 +1,7 @@
-
+SELECT COUNT(e.employee_id)
+FROM employees e
+JOIN departments d ON d.department_id = e.department_id 
+JOIN locations l ON d.location_id = l.location_id
+JOIN countries c ON c.country_id = l.country_id
+JOIN regions r ON c.region_id = r.region_id 
+WHERE r.region_name = 'Europe';
