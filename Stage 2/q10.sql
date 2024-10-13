d05c595ac1c3ee8073e51ced9860b73345f0bc17
Jimmy He <jimmyhe05@gmail.com>
2024-10-13 17:09:39 -0500

    adding an option for using left join.

--- Stage 2/q10.sql
+++ Stage 2/q10.sql
@@ -3,3 +3,9 @@ FROM employees e
 WHERE NOT EXISTS (
     SELECT 1 FROM dependents d WHERE d.employee_id = e.employee_id
 );
+
+-- using left join
+SELECT e.employee_id
+FROM employees e
+LEFT JOIN dependents d ON e.employee_id = d.employee_id
+WHERE d.dependent_id IS NULL;
