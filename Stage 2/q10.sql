710b5b6290625364e633f62c103a836e1834bac0
Jimmy He <jimmyhe05@gmail.com>
2024-10-13 17:24:28 -0500

    using left join.

--- Stage 2/q10.sql
+++ Stage 2/q10.sql
@@ -1,10 +1,3 @@
-SELECT employee_id
-FROM employees e
-WHERE NOT EXISTS (
-    SELECT 1 FROM dependents d WHERE d.employee_id = e.employee_id
-);
-
--- using left join
 SELECT e.employee_id
 FROM employees e
 LEFT JOIN dependents d ON e.employee_id = d.employee_id
